0fdf09289604336ddd2f188960018762f77fd0e2
john <johnzhengyuliu@gmail.com>
2022-07-26 23:22:52 -0500

    updated solution code

--- src/index.ts
+++ src/index.ts
@@ -7,28 +7,32 @@ dotenv.config()
 async function main() {
   // initialize keypair using helper function
   const payer = await initializeKeypair()
+
   // create connection to devnet
   const connection = new anchor.web3.Connection(
     anchor.web3.clusterApiUrl("devnet")
   )
+
+  // call helper function to airdrop SOL to initialized Keypair
   await airdropSol(connection, payer)
 
-  // create Wallet object from payer keypair
+  // create Wallet object from keypair
   const wallet = new anchor.Wallet(payer)
 
-  // create provider from connection and wallet
+  // create AnchorProvider using connection and wallet
   const provider = new anchor.AnchorProvider(connection, wallet, {})
+  // set provider
   anchor.setProvider(provider)
 
-  // convert programId to Publickey object
+  // the on-chain address of the program we will invoke
   const programId = new anchor.web3.PublicKey(
     "3ycJzxn4Akd2A3G2EDAW5RrM5V5DeRWt84MWHJ1ctLUr"
   )
 
-  // create program from IDL and programId
+  // create Program constructor using imported idl and programId
   const program = new anchor.Program(idl as anchor.Idl, programId)
 
-  // generate keypair for counter account
+  // the on-chain address counter account we will be incrementing
   const counter = new anchor.web3.PublicKey(
     "4EgPRavdTMXD42zssM74oAFR8Ea6Upjt2snyNdsiUaFw"
   )
@@ -44,16 +48,25 @@ async function main() {
 }
 
 async function initializeKeypair(): Promise<anchor.web3.Keypair> {
+  // check if keypair in .env file already exists
   if (!process.env.PRIVATE_KEY) {
-    console.log("Creating .env file")
+    console.log("Generating new Keypair")
+    // generate keypair if one does not already exist
     const keypair = anchor.web3.Keypair.generate()
+    console.log("Creating .env file")
+    // create .env file and write secret key from new keypair to .env file
     fs.writeFileSync(".env", `PRIVATE_KEY=[${keypair.secretKey.toString()}]`)
+    // return newly generated keypair
     return keypair
   }
 
+  // read existing keypair in .env file if one already exists
   const secret = JSON.parse(process.env.PRIVATE_KEY ?? "") as number[]
+  // convert to Uint8Array
   const secretKey = Uint8Array.from(secret)
+  // convert to Keypair
   const keypairFromSecretKey = anchor.web3.Keypair.fromSecretKey(secretKey)
+  // return existing keypair
   return keypairFromSecretKey
 }
 
@@ -62,12 +75,14 @@ async function airdropSol(
   payer: anchor.web3.Keypair
 ) {
   console.log("Airdropping 1 SOL...")
+  // requesting airdrop
   const signature = await connection.requestAirdrop(
     payer.publicKey,
     anchor.web3.LAMPORTS_PER_SOL * 1
   )
+  // confirm airdrop transaction
   await connection.confirmTransaction(signature)
-
+  // get new balance after airdrop
   const balance = await connection.getBalance(payer.publicKey)
   console.log("Current balance is", balance / anchor.web3.LAMPORTS_PER_SOL)
 }
@@ -78,7 +93,7 @@ async function incrementInstruction(
   counter: anchor.web3.PublicKey,
   program: anchor.Program
 ) {
-  // invoke "increment" instruction from program
+  // create instruction
   const instruction = await program.methods
     .increment()
     .accounts({
@@ -88,17 +103,20 @@ async function incrementInstruction(
     .signers([payer])
     .instruction()
 
+  // create transaction
   const transaction = new anchor.web3.Transaction().add(instruction)
 
+  // send transaction
   const transactionSignature = await anchor.web3.sendAndConfirmTransaction(
     connection,
     transaction,
     [payer]
   )
+
   await connection.confirmTransaction(transactionSignature)
 
   console.log(
-    `Increment Counter Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    `Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
   )
 }
 
@@ -108,7 +126,7 @@ async function incrementTransaction(
   counter: anchor.web3.PublicKey,
   program: anchor.Program
 ) {
-  // invoke "increment" instruction from program
+  // create transaction
   const transaction = await program.methods
     .increment()
     .accounts({
@@ -118,6 +136,7 @@ async function incrementTransaction(
     .signers([payer])
     .transaction()
 
+  // send transaction
   const transactionSignature = await anchor.web3.sendAndConfirmTransaction(
     connection,
     transaction,
@@ -127,7 +146,7 @@ async function incrementTransaction(
   await connection.confirmTransaction(transactionSignature)
 
   console.log(
-    `Increment Counter Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    `Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
   )
 }
 
@@ -136,7 +155,7 @@ async function incrementRpc(
   counter: anchor.web3.PublicKey,
   program: anchor.Program
 ) {
-  // invoke "increment" instruction from program
+  // send transaction
   const transactionSignature = await program.methods
     .increment()
     .accounts({
@@ -147,7 +166,7 @@ async function incrementRpc(
     .rpc()
 
   console.log(
-    `Increment Counter Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+    `Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
   )
 }
 
