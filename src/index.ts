8b223af6140aaabb5a8225d38d03a061fff908dc
john <johnzhengyuliu@gmail.com>
2022-07-26 22:23:36 -0500

    update starter

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,7 @@
 import * as anchor from "@project-serum/anchor"
 import idl from "./idl.json"
 
-async function main() {
-
-}
-
-async function airdrop(
-  connection: anchor.web3.Connection,
-  payer: anchor.web3.Keypair
-) {
-
-}
-
-async function createCounter(
-  wallet: anchor.Wallet,
-  counter: anchor.web3.Keypair,
-  program: anchor.Program
-) {
-
-}
-
-async function incrementCounter(
-  wallet: anchor.Wallet,
-  counter: anchor.web3.Keypair,
-  program: anchor.Program
-) {
-
-}
+async function main() {}
 
 main()
   .then(() => {
